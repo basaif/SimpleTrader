443d495cb4dd9aff7c5a476ce6e99092862a8e87
basaif <iman.basaif@gmail.com>
2022-05-22 18:24:55 +0300

    Add project files.

---
+++ SimpleTrader.Domain/Models/Stock.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleTrader.Domain.Models
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public double PricePerShare { get; set; }
+
+    }
+}
---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+        public SimpleTraderDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Account>? Accounts { get; set; }
+        public DbSet<AssetTransaction>? AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

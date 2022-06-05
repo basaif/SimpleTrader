7e4b3342abb54bb79b9812b49da0bebef95593ad
basaif <iman.basaif@gmail.com>
2022-06-05 21:05:12 +0300

    Add Generic Host
    Migrations don't work

--- SimpleTrader.WPF/App.xaml.cs
+++ SimpleTrader.WPF/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using SimpleTrader.Domain.Models;
 using SimpleTrader.Domain.Services;
 using SimpleTrader.Domain.Services.AuthenticationServices;
@@ -32,91 +34,101 @@ namespace SimpleTrader.WPF
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = CreateHostBuilder().Build();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args = null)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(c =>
+                {
+                    c.AddJsonFile("appsettings.json");
+                    c.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    string connectionString = context.Configuration.GetConnectionString("default");
+                    string apiKey = context.Configuration.GetValue<string>("ApiKey");
+
+                    services.AddDbContext<SimpleTraderDbContext>(o => o.UseSqlServer(connectionString));
+                    services.AddSingleton<SimpleTraderDbContextFactory>(new SimpleTraderDbContextFactory(connectionString));
+                    services.AddSingleton(services =>
+                    {
+                        return new FinancialModelingPrepHttpClientFactory(apiKey);
+                    });
+
+                    services.AddSingleton<IDataService<Account>, AccountDataService>();
+                    services.AddSingleton<IAccountService, AccountDataService>();
+                    services.AddSingleton<IAuthenticationService, AuthenticationService>();
+                    services.AddSingleton<IStockPriceService, DummyStockPriceService>();
+                    services.AddSingleton<IBuyStockService, BuyStockService>();
+                    services.AddSingleton<IMajorIndexService, MajorIndexService>();
+
+                    services.AddSingleton<IPasswordHasher, PasswordHasher>();
+
+                    services.AddSingleton<IViewModelFactory, ViewModelFactory>();
+                    services.AddSingleton<BuyViewModel>();
+                    services.AddSingleton<PortfolioViewModel>();
+                    services.AddSingleton<AssetSummaryViewModel>();
+                    services.AddSingleton(services =>
+                        new HomeViewModel(
+                            services.GetRequiredService<AssetSummaryViewModel>(),
+                            MajorIndexListingViewModel
+                            .LoadMajorIndexViewModel(services.GetRequiredService<IMajorIndexService>()))
+                    );
+
+                    services.AddSingleton<CreateViewModel<HomeViewModel>>(services =>
+                    {
+                        return () => services.GetRequiredService<HomeViewModel>();
+                    });
+                    services.AddSingleton<CreateViewModel<BuyViewModel>>(services =>
+                    {
+                        return () => services.GetRequiredService<BuyViewModel>();
+                    });
+                    services.AddSingleton<CreateViewModel<PortfolioViewModel>>(services =>
+                    {
+                        return () => services.GetRequiredService<PortfolioViewModel>();
+                    });
+
+                    services.AddSingleton<ViewModelRenavigator<HomeViewModel>>();
+
+                    services.AddSingleton<CreateViewModel<LoginViewModel>>(services =>
+                    {
+                        return () => new LoginViewModel(services.GetRequiredService<IAuthenticator>(),
+                            services.GetRequiredService<ViewModelRenavigator<HomeViewModel>>());
+                    });
+
+                    services.AddScoped<MainViewModel>();
+                    services.AddScoped<BuyViewModel>();
+                    services.AddSingleton<INavigator, Navigator>();
+                    services.AddSingleton<IAuthenticator, Authenticator>();
+                    services.AddSingleton<IAccountStore, AccountStore>();
+                    services.AddSingleton<AssetStore>();
+
+                    services.AddScoped(s => new MainWindow(s.GetRequiredService<MainViewModel>()));
+                });
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            IServiceProvider serviceProvider = CreateServiceProvider();
-            Window window = serviceProvider.GetRequiredService<MainWindow>();
+            _host.Start();
+
+            Window window = _host.Services.GetRequiredService<MainWindow>();
          
             window.Show();
             base.OnStartup(e);
         }
 
-        private static IConfiguration AddConfiguration()
+        protected override async void OnExit(ExitEventArgs e)
         {
-            IConfigurationBuilder builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
-            builder.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
-
-            return builder.Build();
+            await _host.StopAsync();
+            _host.Dispose();
+            base.OnExit(e);
         }
 
-        private static IServiceProvider CreateServiceProvider()
-        {
-            IServiceCollection services = new ServiceCollection();
-            
-            services.AddSingleton(AddConfiguration());
-
-            
-            services.AddSingleton(services =>
-            {
-                IConfiguration config = services.GetRequiredService<IConfiguration>();
-                string apiKey = config.GetValue<string>("ApiKey");
-                return new FinancialModelingPrepHttpClientFactory(apiKey);
-            });
-
-            services.AddSingleton<SimpleTraderDbContextFactory>();
-            services.AddSingleton<IDataService<Account>, AccountDataService>();
-            services.AddSingleton<IAccountService, AccountDataService>();
-            services.AddSingleton<IAuthenticationService, AuthenticationService>();
-            services.AddSingleton<IStockPriceService, DummyStockPriceService>();
-            services.AddSingleton<IBuyStockService, BuyStockService>();
-            services.AddSingleton<IMajorIndexService, MajorIndexService>();
-
-            services.AddSingleton<IPasswordHasher, PasswordHasher>();
-
-            services.AddSingleton<IViewModelFactory, ViewModelFactory>();
-            services.AddSingleton<BuyViewModel>();
-            services.AddSingleton<PortfolioViewModel>();
-            services.AddSingleton<AssetSummaryViewModel>();
-            services.AddSingleton(services =>
-                new HomeViewModel(
-                    services.GetRequiredService<AssetSummaryViewModel>(),
-                    MajorIndexListingViewModel
-                    .LoadMajorIndexViewModel(services.GetRequiredService<IMajorIndexService>()))
-            );
-
-            services.AddSingleton<CreateViewModel<HomeViewModel>>(services =>
-            {
-                return () => services.GetRequiredService<HomeViewModel>();
-            });
-            services.AddSingleton<CreateViewModel<BuyViewModel>>(services =>
-            {
-                return () => services.GetRequiredService<BuyViewModel>();
-            });
-            services.AddSingleton<CreateViewModel<PortfolioViewModel>>(services =>
-            {
-                return () => services.GetRequiredService<PortfolioViewModel>();
-            });
-
-            services.AddSingleton<ViewModelRenavigator<HomeViewModel>>();
-
-            services.AddSingleton<CreateViewModel<LoginViewModel>>(services =>
-            {
-                return () => new LoginViewModel(services.GetRequiredService<IAuthenticator>(),
-                    services.GetRequiredService<ViewModelRenavigator<HomeViewModel>>());
-            });
-
-            services.AddScoped<MainViewModel>();
-            services.AddScoped<BuyViewModel>();
-            services.AddSingleton<INavigator, Navigator>();
-            services.AddSingleton<IAuthenticator, Authenticator>();
-            services.AddSingleton<IAccountStore, AccountStore>();
-            services.AddSingleton<AssetStore>();
-
-            services.AddScoped(s => new MainWindow(s.GetRequiredService<MainViewModel>()));
-
-            return services.BuildServiceProvider();
-        }
     }
 }

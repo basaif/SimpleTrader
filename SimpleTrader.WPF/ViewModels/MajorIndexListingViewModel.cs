3e5dd7e5c7c1651cd04d00c519073f83c6be7be8
basaif <iman.basaif@gmail.com>
2022-06-12 19:37:01 +0300

    Add Loading Spinner for Major Indexes

--- SimpleTrader.WPF/ViewModels/MajorIndexListingViewModel.cs
+++ SimpleTrader.WPF/ViewModels/MajorIndexListingViewModel.cs
@@ -1,24 +1,20 @@
 ﻿using SimpleTrader.Domain.Models;
 using SimpleTrader.Domain.Services;
+using SimpleTrader.WPF.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SimpleTrader.WPF.ViewModels
 {
     public class MajorIndexListingViewModel : ViewModelBase
     {
-        private readonly IMajorIndexService _majorIndexService;
         private MajorIndex _dowJones = new();
         private MajorIndex _nasdaq = new();
         private MajorIndex _sP500 = new();
-
-        public MajorIndexListingViewModel(IMajorIndexService majorIndexService)
-        {
-            _majorIndexService = majorIndexService;
-        }
 
         public MajorIndex DowJones
         {
@@ -46,6 +42,21 @@ namespace SimpleTrader.WPF.ViewModels
                 OnPropertyChanged(nameof(Nasdaq));
             }
         }
+
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get
+            {
+                return _isLoading;
+            }
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged(nameof(IsLoading));
+            }
+        }
+
         public MajorIndex SP500
         {
             get
@@ -59,35 +70,19 @@ namespace SimpleTrader.WPF.ViewModels
                 OnPropertyChanged(nameof(SP500));
             }
         }
+        public ICommand LoadMajorIndexesCommand { get; }
+        public MajorIndexListingViewModel(IMajorIndexService majorIndexService)
+        {
+            LoadMajorIndexesCommand = new LoadMajorIndexesCommand(this, majorIndexService);
+        }
+
         public static MajorIndexListingViewModel LoadMajorIndexViewModel(IMajorIndexService majorIndexService)
         {
             MajorIndexListingViewModel majorIndexViewModel = new(majorIndexService);
-            majorIndexViewModel.LoadMajorIndexes();
+
+            majorIndexViewModel.LoadMajorIndexesCommand.Execute(null);
+
             return majorIndexViewModel;
-        }
-        private void LoadMajorIndexes()
-        {
-            _majorIndexService.GetMajorIndex(MajorIndexType.DowJones).ContinueWith(task =>
-            {
-                if (task.Exception is null)
-                {
-                    DowJones = task.Result;
-                }
-            });
-            _majorIndexService.GetMajorIndex(MajorIndexType.Nasdaq).ContinueWith(task =>
-            {
-                if (task.Exception is null)
-                {
-                    Nasdaq = task.Result;
-                }
-            });
-            _majorIndexService.GetMajorIndex(MajorIndexType.SP500).ContinueWith(task =>
-            {
-                if (task.Exception is null)
-                {
-                    SP500 = task.Result;
-                }
-            });
         }
     }
 }
